7e47f850e54680296a3630f698ac193954e4852f
Your Name <you@example.com>
2018-12-26 18:18:13 -0600

    Add login funcionality

--- blog.py
+++ blog.py
@@ -6,6 +6,9 @@ import sqlite3
 
 # configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'hard_to_guess'
 app = Flask(__name__)
 
 # pulls in app configuration by looking for UPPERCASE variables
@@ -14,6 +17,30 @@ app.config.from_object(__name__)
 # function used for connecting to the database
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
+
+@app.route('/', methods=['GET', 'POST']) # add methods argument to decorator to use POST request
+def login():
+	error = None
+	status_code = 22
+	# Specify POST request
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid Credentials. Please try again.'
+			status_code = 401
+		else:
+			session['logged_in'] = True
+			return redirect(url_for('main')) # url_for() function generates an endpoint for the provided method
+	return render_template('login.html')
+
+@app.route('/main')
+def main():
+	return render_template('main.html')
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('login'))
 
 if __name__ == '__main__':
 	app.run(debug=True)
